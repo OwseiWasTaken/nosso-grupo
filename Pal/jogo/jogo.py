ad20e9687ca549099f5806621d87d85ca7784401
owsei <owseiwastaken@gmail.com>
2022-11-04 21:55:08 -0300

    comments

--- Pal/jogo/jogo.py
+++ Pal/jogo/jogo.py
@@ -16,13 +16,16 @@ class Video:
 
 def MakeVids() -> list[Video]:
 	vids = []
+	# open file, join lines with ; and split lines with ;; (\n\n)
 	with open("./vids.txt", 'r') as f:
 		fvids = ';'.join(list(map(
 			lambda x:(x.strip()),
 			f.readlines()
 			))).split(';;')
+	# make tuple of each video (based on ;)
 	fvids = [tuple(x.split(';')) for x in fvids]
 	cvids = []
+	# set choice -> name for each vid line (except self's name)
 	for vid in fvids:
 		d = {}
 		if len(vid) != 1:
@@ -34,14 +37,16 @@ def MakeVids() -> list[Video]:
 	return cvids
 
 def LinkVids(vids: list[Video]) -> list[Video]:
+	# make lookup table obj.name->obj
 	d = {x.vidname:x for x in vids}
+	# for each vif
 	for vid in vids:
+		# set value (name) of choice[k] as obj (based on name->obj)
 		vid.choices = {k:d[v] for k, v in vid.choices.items()}
 	return vids
 
 def main() -> str:
-	vids = MakeVids()
-	LinkVids(vids)
+	vids = LinkVids(MakeVids())
 	for vid in vids:
 		print(repr(vid))
 	return ""

040444d34bee4e03612b165549d285382141d063
Pedro Manse <pedro.manse@dmk3.com.br>
2023-05-15 21:29:52 -0300

    structure for TagTree, HasContent/Child is now method of Tag

--- PiaCheia/server/mml/reader.go
+++ PiaCheia/server/mml/reader.go
@@ -12,27 +12,39 @@ type Tag struct {
 	Name string
 	Attributes Obj
 	Content string
-	HasContent bool
 	Children []*Tag
-	HasChild bool
 	Depth int
 }
 
-func (T *Tag) MakeTag(name string, attrs Obj) (Tag) {
+func (T Tag) Tab(i... int) (string) {
+	if len(i) == 0 {
+		return strings.Repeat("\t", T.Depth);
+	} else {
+		return strings.Repeat("\t", T.Depth+i[0]);
+	}
+}
+
+func (T Tag) HasContent() (bool) {
+	return len(T.Content) != 0
+}
+
+func (T Tag) HasChild() (bool) {
+	return len(T.Children) != 0
+}
+
+func (T *Tag) MakeTag(name string, attrs Obj) (*Tag) {
 	var NewNode Tag = Tag{
 		Name:name,
 		Attributes:attrs,
 		Content:"",
-		HasContent: false,
 		Children:[]*Tag{},
-		HasChild:false,
 		Depth: T.Depth+1,
 	}
 	index := T.InsertTag(&NewNode)
 	if id, ok := attrs["id"]; ok {
 		TagMap[id] = T.Children[index]
 	}
-	return NewNode
+	return &NewNode
 }
 
 func (T Tag) formatAttrs() (text string) {
@@ -43,47 +55,58 @@ func (T Tag) formatAttrs() (text string) {
 }
 
 func (T Tag) innerHTML() (text string) {
-	if (!(T.HasContent||T.HasChild)) {return ""}
-	if (T.HasContent) {return T.Content+"\n</"+T.Name+">"}
+	if (T.HasContent()) {return "\n"+T.Tab(1)+T.Content+"\n"+T.Tab()+"</"+T.Name+">"}
 
 	for _, tag := range T.Children {
 		text+=tag.Format()
 	}
-	text+="\n</"+T.Name+">"
+	text+="\n"+T.Tab()+"</"+T.Name+">"
 	return text
 }
 
 func (T Tag) Format() (string) {
 	return fmt.Sprintf("\n%s<%s%s>%s",
-		strings.Repeat("\t", T.Depth),
+		T.Tab(),
 		T.Name,
 		T.formatAttrs(),
 		T.innerHTML(),
 	)
 }
 
-func Root() (T Tag) {
-	return Tag{
-		Name:"html",
-		Attributes:map[string]string{},
-		Content:"",
-		HasContent: false,
-		Children:[]*Tag{},
-		HasChild:true,
-		Depth: 0,
-	}
-}
-
 func (T *Tag) InsertTag(Child *Tag) (int){
-	T.HasChild = true
 	T.Children = append(T.Children, Child)
 	return len(T.Children)-1
 }
 
+// return last tag
+func (T *Tag) TagTree(Objects... Obj) (*Tag) {
+	var current = T
+	var name string
+	for _, obj := range Objects {
+		name = obj["name"] //TODO assert tag has name
+		delete(obj, "name")
+		current = current.MakeTag(name, obj)
+	}
+	return T
+}
+
 var TagMap = map[Id]*Tag{}
-var Document = Root()
+var Document = Tag{
+		Name:"html",
+		Attributes:map[string]string{},
+		Content:"",
+		Children:[]*Tag{},
+		Depth: 0,
+}
+
 func MM() {
-	Document.MakeTag("body", Obj{"id":"uwud"})
-	fmt.Println(Document)
+	// body#uwu;
+	body := Document.MakeTag("body", Obj{"id":"uwu"})
+	// #uwu->ul->li->p#texthere
+	body.TagTree(Obj{"name":"ul"}, Obj{"name":"li"}, Obj{"name":"p", "id":"texthere"})
+	// #texthere { text; class:"awhn" }
+	TagMap["texthere"].Content = "text"
+	TagMap["texthere"].Attributes["class"] = "awhn"
+
 	fmt.Println(Document.Format())
 }

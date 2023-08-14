97a8a44c0e0f81eb44009cee7598f7a88bcecb35
Pedro Manse <pedro.manse@dmk3.com.br>
2023-08-14 17:55:02 -0300

    fclose/fopen better convert.sh

--- PiaCheia/pages/convert.sh
+++ PiaCheia/pages/convert.sh
@@ -2,30 +2,44 @@
 
 set -e
 
-echo -ne '<DOCTYPE html>
+for md_file in $@
+do
+	# remove .md from filename
+	page_path=$(echo $md_file | sed 's|\(.*\)\.md|\1|')
+	# remove everything until last /
+	page_name=$(basename $page_path)
+	html_file="$page_path.html"
+
+	echo -ne "<!DOCTYPE html>
+
 <html>
 <head>
-	<link rel="stylesheet" href="../style.css">
+	<title>$page_name</title>
+	<link rel=\"stylesheet\" href=\"../style.css\">
 </head>
 <body>
-'
+" > $html_file
 
-cat $1 \
-| perl -pe 's|\b_(.*?)_\b|<i>\1</i>|g' \
-\
-| sed 's|###### \(.*\?\) ######|<h6>\1</h6>|g' \
-| sed 's|##### \(.*\?\) #####|<h5>\1</h5>|g' \
-| sed 's|#### \(.*\?\) ####|<h4>\1</h4>|g' \
-| sed 's|### \(.*\?\) ###|<h3>\1</h3>|g' \
-| sed 's|## \(.*\?\) ##|<h2>\1</h2>|g' \
-| sed 's|# \(.*\?\) #|<h1>\1</h1>|g' \
-\
-| sed 's|\(\w*\)\?`\(.*\?\)`|<code class="\1">\2</code>|g' \
-\
-| sed 's|;;|<br>|' \
-| sed 's|;$|<br>|' \
-| sed 's|^$|<br>|' \
-| sed 's|\
-|<br>|'
+	cat $md_file \
+	| sed 's|<|\&lt;|g;s|>|\&gt;|g' \
+	| sed 's|^{$|<div>|g;s|^}$|</div>|g' \
+	| perl -pe 's|\b_(.*?)_\((.*?)\)|<span><i>\1</i><i class="popup">\2</i></span>|g' \
+	| perl -pe 's|\[(.*?)\]\((.*?)\)|<a href\="\2">_\1_</a>|g' \
+	| perl -pe 's|(\s)\*(.*?)\*(\s)|\1<strong>\2</strong>\3|g' \
+	| perl -pe 's|\b_(.*?)_\b|<i>\1</i>|g' \
+	| sed 's|###### \(.*\?\) ######|<h6>\1</h6>|g' \
+	| sed 's|##### \(.*\?\) #####|<h5>\1</h5>|g' \
+	| sed 's|#### \(.*\?\) ####|<h4>\1</h4>|g' \
+	| sed 's|### \(.*\?\) ###|<h3>\1</h3>|g' \
+	| sed 's|## \(.*\?\) ##|<h2>\1</h2>|g' \
+	| sed 's|# \(.*\?\) #|<h1>\1</h1>|g' \
+	| sed 's|\(\w*\)\?`\(.*\?\)`|<code class="\1">\2</code>|g' \
+	| sed 's|;;|<br>|;s|;$|<br>|;s|^$|<br>|' \
+	| sed 's|\
+|<br>|' >> $html_file
 
-echo -ne "</body></html>"
+	echo -ne "</body></html>" >> $html_file
+
+	echo "converted $md_file -> $html_file"
+done
+

826f7e5b3b2d3606f2e6972bb182c76ab5021800
Pedro Manse <pedro.manse@dmk3.com.br>
2023-08-29 18:04:48 -0300

    servidor dvd

---
+++ PiaCheia/servidor/files/js/fs.js
@@ -0,0 +1,291 @@
+"use strict";
+
+class Dir {
+	constructor(filename) {
+		this.isDir = true;
+		this.UnresolvedName = filename
+		filename = FS_ShrinkPath(filename)
+		this.fsEndpoint = "/fs/"+filename
+		this.filesEndpoint = "/files/"+filename
+		this.filename = filename
+		this.fullname = BaseName(filename)
+		this.name = filename.split(".").slice(0, -1).join(".")
+		this.path = PathName(filename)
+		this.files = [];
+		this.fileMap = {};
+		this.size = 4096;
+		this.modTime = new Date();
+	}
+	push(file) {
+		this.files.push(file.fullname)
+		this.fileMap[file.fullname] = file
+	}
+	get(filename) {
+		return this.fileMap[filename]
+	}
+	forEach(fnc) {
+		this.files.forEach(fl=>{
+			fnc(this.fileMap[fl])
+		})
+	}
+	filter(fnc) {
+		return this.files.filter(fl=>{
+			return fnc(this.fileMap[fl])
+		}).map((name)=>this.fileMap[name])
+	}
+}
+
+class File {
+	constructor(filename) {
+		this.isDir = false;
+		this.UnresolvedName = filename
+		filename = FS_ShrinkPath(filename)
+		this.fsEndpoint = "/fs/"+filename
+		this.filesEndpoint = "/files/"+filename
+		this.filename = filename
+		this.fullname = BaseName(filename)
+		this.name = BaseName(filename).split(".").slice(0, -1).join(".")
+		this.extension = BaseName(filename).split(".").at(-1)
+		this.path = PathName(filename)
+		this.size = this.fullname.length;
+		this.modTime = new Date();
+	}
+}
+
+function _FS_walk(tree, path) {
+	if (path.length === 0) return tree;
+	//tree = tree[path[0]];
+	tree = tree.fileMap[path[0]];
+	return _FS_walk(tree, path.slice(1));
+}
+
+function IsDir(path) {
+	return path.substr(path.length - 1) === '/';
+}
+
+function StripLastSlash(path) {
+	if (IsDir(path)) {
+		path = path.substr(0,path.length-1);
+	}
+	return path;
+}
+
+// ./pages/./here/../there -> pages/there
+// "./" -> ""
+function FS_ShrinkPath(path) {
+	return path
+		.replaceCertain(/\/\//g, "/") // "//" -> "/"
+		.replaceCertain(/[^/]*\/\.\./g, "/") // "*/.." -> ""
+		.replaceCertain(/\/\.\//g, "/") // "/." -> "/"
+		.replace(/^\.?\//, "")
+		.replace(/\/$/, "")
+}
+
+function FS_aproach(tree, path) {
+	path = FS_ShrinkPath(path)
+	path = path.split("/");
+	path.length--;
+	return _FS_walk(tree, path);
+}
+
+function FS_walk(tree, path) {
+	path = FS_ShrinkPath(path)
+	if (!path) return tree;
+	path = path.split("/");
+	return _FS_walk(tree, path);
+}
+
+function PathName(path) {
+	path = StripLastSlash(path)
+	path = path.split("/");
+	path.length--;
+	return path;
+}
+
+function BaseName(path) {
+	path = StripLastSlash(path)
+	path = path.split("/");
+	return path.at(-1);
+}
+
+function BuildFilsSystem(PathRes) {
+	const ROOT = new Dir("/");
+	PathRes.forEach((path)=>{
+		if (IsDir(path)) {
+			FS_aproach(ROOT, path).push(new Dir(path));
+		} else {
+			FS_aproach(ROOT, path).push(new File(path));
+		}
+	})
+	return ROOT;
+}
+
+const FS_REMIRROR = async () => {
+	const ret = await fetch("/fs/").then(a=>a.json())
+	FS_MIRROR_PATH = () => {
+		return ret;
+	}
+	return ret;
+}
+
+let FS_MIRROR_PATH = () => {
+	throw "FS_REMIRROR must be ran before FS_MIRROR_PATH";
+}
+
+function SelectFile(callback, CWD="", opt={}) {
+	const FS = BuildFilsSystem(FS_MIRROR_PATH())
+	return FS_redraw( null, FS, CWD, callback, {
+		createFile:true, createFolder:true, ...opt
+	})
+}
+
+// example of file selector
+// after uses selects file, they are redirected
+// to it's /files/ endPoint
+//window.onload = async () => {
+//	await FS_REMIRROR();
+//	SelectFile((file, event)=>{
+//		window.location = file.filesEndpoint
+//	});
+//}
+
+const FS_default_draw_options = {
+	confirmSelect: false, //TODO: implement double-click confirmation
+	selectFile:true, //TODO: implement not selecting files
+	selectFolder:false, // TODO: implement folder selection/walking
+	createFile:false, // TODO: implement server-mirroring
+	createFolder:false, // TODO: implement server-mirroring
+}
+
+function FS_redraw(
+	oldDialog, FS, CWD,
+	callback, options=FS_default_draw_options
+) {
+
+	// define unset option 'a as FS_default_draw_options['a]
+	options = {...FS_default_draw_options, ...options};
+	let Dialog = null;
+	if (oldDialog === null) {
+		Dialog = hCreateElement("dialog", {
+			class:"FS-file-select-dialog"
+		}, [
+			createElement("h2", "At /"+CWD, {
+				style:{
+					position:"relative",
+					display:"inline-block",
+					bottom:"20px",
+					margin:0,
+					right:"10px",
+				}
+			}),
+			hCreateElement("table", [
+				hCreateElement("thead", [
+					hCreateElement("tr", [
+						createElement("th", "Nome"),
+						createElement("th", "Endpoint"),
+						createElement("th", "Tamanho"),
+						createElement("th", "Última Modificação"),
+					]),
+				]),
+				createElement("tbody"),
+			]),
+			hCreateElement("div", {id:"FS-bottom"}, [
+				createElement("input", {id:"FS-search"}),
+				createElement("button", {id:"FS-select"}, "Selecionar"),
+				createElement("button", {id:"FS-create-file"}, "Novo Arquivo"),
+				createElement("button", {id:"FS-create-folder"}, "Nova Pasta"),
+			]),
+		])
+		document.body.appendChild(Dialog)
+		Dialog.showModal()
+	} else {
+		Dialog = oldDialog;
+		cquery(oldDialog, "tbody").innerHTML = "";
+		cquery(oldDialog, "h2").innerText = "At /"+CWD;
+	}
+
+	const tbody = cquery(Dialog, "tbody");
+	let makeFile = cquery(Dialog, "#FS-create-file");
+	let makeFolder = cquery(Dialog, "#FS-create-folder");
+
+	makeFile = RemoveListeners(makeFile);
+	makeFolder = RemoveListeners(makeFolder);
+
+	makeFile.disabled = !options.createFile;
+	makeFolder.disabled = !options.createFolder;
+
+	//TODO actually make a folder or file
+	makeFile.addEventListener("click", (event)=>{
+		const FileName = CWD+"/"+prompt("Nome para novo arquivo");
+		FS_walk(FS, CWD).push(new File(FileName))
+		FS_redraw(Dialog, FS, CWD, callback, options)
+	})
+	makeFolder.addEventListener("click", (event)=>{
+		const FolderName = CWD+"/"+prompt("Nome para nova pasta")+"/";
+		FS_walk(FS, CWD).push(new Dir(FolderName))
+		FS_redraw(Dialog, FS, CWD, callback, options)
+	});
+
+	if (CWD) {
+		const upDir = new Dir(CWD+"/..")
+		upDir.fullname = "..";
+		tbody.appendChild( FS_TROW(upDir, ()=>{
+			FS_redraw(Dialog, FS, upDir.filename, callback, options)
+		}))
+	}
+
+	FS_walk(FS, CWD)
+		.filter( ({isDir}) => isDir )
+		.forEach(folder=>{
+		tbody.appendChild( FS_TROW(folder, ()=>{
+			FS_redraw(Dialog, FS, CWD+"/"+folder.fullname, callback, options)
+		}))
+	})
+
+	FS_walk(FS, CWD)
+		.filter( ({isDir}) => !isDir )
+		.forEach(file=>{
+		tbody.appendChild( FS_TROW(file, (e)=>{
+			Dialog.close();
+			callback(file, e);
+		}))
+	})
+	return Dialog;
+}
+
+function FS_TROW(fileOrDir, Click) {
+	const name = createElement("button", fileOrDir.fullname, {
+		class:"FS-btn-like-link",
+		style:{
+			width:"120%",
+			position:"relative",
+			right:"10px",
+		}
+	})
+	name.classList.add(fileOrDir.isDir?"FS-dir":"FS-file")
+	name.addEventListener("click", Click)
+
+	return hCreateElement("tr", [
+		name,
+		createElement("td", fileOrDir.filesEndpoint),
+		createElement("td", fileOrDir.size),
+		createElement("td", UnixFormatDate(fileOrDir.modTime)),
+	])
+}
+
+function MakeFile(fsEndpoint, name, content="") {
+	console.error(`Make File ${name} @ ${fsEndpoint} "${content}" `)
+}
+
+function MakeFolder(fsEndpoint, name) {
+	console.error(`Make Folder ${name} @ ${fsEndpoint}`)
+}
+
+function LoadFile(filesEndpoint) {
+	return fetch(filesEndpoint, {cache: "no-cache"}).then(a=>a.text())
+}
+
+function LoadJson(filesEndpoint) {
+	return fetch(filesEndpoint, {cache: "no-cache"}).then(a=>a.json())
+}
+
---
+++ PiaCheia/servidor/main.go
@@ -0,0 +1,81 @@
+package main
+
+import (
+	"strings"
+	"flag"
+	"fmt"
+	"encoding/json"
+	"io/fs"
+	"net/http"
+	"path/filepath"
+)
+
+var ( // flags
+	ADDR string
+)
+
+type StaticPage struct{ FileName string }
+func (p StaticPage) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	http.ServeFile(w, r, p.FileName)
+}
+
+func FileSystemAPI(w http.ResponseWriter, r *http.Request) {
+	file := r.URL.Path[4:]
+
+	if (file == "") {
+		w.Write(Unpack(json.Marshal(Ls("files/"))))
+		return
+	}
+
+}
+
+func init() {
+	flag.StringVar(&ADDR, "addr", "127.0.0.1:80", "endereço de ip do servidor")
+	flag.Parse()
+}
+
+func main() {
+	http.Handle("/", StaticPage{"./files/main.html"})
+	http.Handle("/convert", StaticPage{"./files/convert.html"})
+
+	http.Handle("/articles/", http.StripPrefix("/articles/", http.FileServer(http.Dir("files/articles/"))))
+	http.Handle("/files/", http.StripPrefix("/files/", http.FileServer(http.Dir("files/"))))
+	http.HandleFunc("/fs/", FileSystemAPI)
+	http.HandleFunc("/art/", ArticleSystemAPI)
+
+	fmt.Println("running")
+
+	panic(http.ListenAndServe(ADDR, nil))
+}
+
+func Ls(dir string) []string {
+	var files = []string{}
+	Panic(filepath.Walk(dir, func(path string, info fs.FileInfo, err error) error { // download
+		if (dir == path) {return nil}
+		path = strings.TrimPrefix(path, dir)
+
+		if ( info.IsDir() && !strings.HasSuffix(path, "/")) {
+			path+="/"
+		}
+		if err != nil {
+			fmt.Printf("Can't access file from path %q; e: %v\n", path, err)
+			return err
+		}
+		files = append(files, path)
+		return nil
+	}))
+	return files
+}
+
+func Panic(e error) {
+	if (e != nil) {
+		panic(e)
+	}
+}
+
+func Unpack[T any](v T, e error) T {
+	if (e != nil) {
+		panic(e)
+	}
+	return v
+}
